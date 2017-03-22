00e8c76ea561b819ec9d76a67919b4439f96da08
apple <963182316@qq.com>
2017-03-22 21:54:18 +0800

    1

--- app/src/main/java/com/example/apple/android_design/MainActivity.java
+++ app/src/main/java/com/example/apple/android_design/MainActivity.java
@@ -2,28 +2,78 @@ package com.example.apple.android_design;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import static com.example.apple.android_design.R.id.view;
+import static com.example.apple.android_design.R.menu.toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
-    @Override
+
+    private  Fruit[]fruits={new Fruit("苹果呀",R.drawable.apple),
+                            new Fruit("香蕉呀",R.drawable.banana),
+                            new Fruit("橘子呀",R.drawable.orange),
+                            new Fruit("西瓜呀",R.drawable.watermelon),
+                            new Fruit("葡萄呀",R.drawable.pear),
+                            new Fruit("葡萄呀",R.drawable.grape),
+                            new Fruit("菠萝呀",R.drawable.pineapple),
+                            new Fruit("草莓呀",R.drawable.strawberry),
+                            new Fruit("樱桃呀",R.drawable.cherry),
+                            new Fruit("芒果呀",R.drawable.mango)    };
+    private List<Fruit>fruitList=new ArrayList<>();
+    private FruitAdapter adapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initFruit();
+        RecyclerView recyclerView= (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager=new GridLayoutManager(this,2);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter=new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toobar);
         setSupportActionBar(toolbar);
         mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_loyout);
-        NavigationView navigationView= (NavigationView) findViewById(R.id.view);
+        NavigationView navigationView= (NavigationView) findViewById(view);
         ActionBar actionBar=getSupportActionBar();
+
+        FloatingActionButton fab= (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v,"Data deleted",Snackbar.LENGTH_SHORT).
+                        setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                ToastUtil.showToast(MainActivity.this,"亲，点我干啥");
+                            }
+                        })
+                        .show();
+            }
+        });
+
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
             actionBar.setHomeAsUpIndicator(R.mipmap.menu);
@@ -39,9 +89,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initFruit() {
+        fruitList.clear();
+        for (int i=0;i<50;i++){
+            Random random=new Random();
+            int index=random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar, menu);
+        getMenuInflater().inflate(toolbar, menu);
         return true;
     }
 
@@ -53,18 +112,15 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.backup:
-                Toast.makeText
-                        (this, "你是不是傻，点击了备份", Toast.LENGTH_SHORT).show();
+                ToastUtil.showToast(MainActivity.this,"你是不是傻，点击了备份");
                 break;
 
             case R.id.delete:
-                Toast.makeText
-                        (this, "你很棒棒，点击了删除", Toast.LENGTH_SHORT).show();
+                ToastUtil.showToast(MainActivity.this,"你很棒棒，点击了删除");
                 break;
 
             case R.id.setings:
-                Toast.makeText
-                        (this, "你知道的太多了，点击了设置", Toast.LENGTH_SHORT).show();
+                ToastUtil.showToast(MainActivity.this,"你知道的太多了，点击了设置");
                 break;
             default:
 
